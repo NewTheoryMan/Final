58b49c3e53cdb19ba8f0e53a6e55ed3f9dd1b14b
Chad Townes <chad.townes@gmail.com>
2013-12-10 03:51:16 -0800

    updated references page, added javascript to links

--- JavaScript.js
+++ JavaScript.js
@@ -1,3 +1,7 @@
-﻿function myFunction() {
-    document.getElementById("Git").innerHTML = "My First JavaScript Function";
+﻿var aElems = document.getElementsByTagName('confirmation');
+
+for (var i = 0, len = aElems.length; i < len; i++) {
+    aElems[i].onclick = function () {
+        return confirm("Are you sure you want to leave?");
+    };
 }
